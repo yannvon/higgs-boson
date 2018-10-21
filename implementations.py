82a2b81e588abe856bc7e28c76a290eb53bd9191
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2018-10-21 16:51:46 +0200

    Add dataset zip and python file

---
+++ implementations.py
@@ -0,0 +1,23 @@
+# ----- Implement ML methods -------------------------------------------------------------------------
+
+# Linear regression using gradient descent
+least squares GD(y, tx, initial w, max iters, gamma)
+
+# Linear regression using stochastic gradient descent
+least squares SGD(y, tx, initial w, max iters, gamma)
+
+# Least squares regression using normal equations
+least squares(y, tx)
+
+# Ridge regression using normal equations
+ridge regression(y, tx, lambda_ )
+
+# Logistic regression using gradient descent or SGD
+logistic regression(y, tx, initial w, max iters, gamma)
+
+# Regularized logistic regression using gradient descent or SGD
+reg logistic regression(y, tx, lambda_ , initial w, max iters, gamma)
+
+
+
+# ----- Additional methods ---------------------------------------------------------------------------
